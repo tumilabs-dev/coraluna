8b7da7cae59b425a4ebc7b830c14cd686ab166b5
krist.dev.vn <krist.dev.vn@gmail.com>
2025-12-05 18:09:25 +0700

    Add meta tags for SEO and viewport settings in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -81,6 +81,35 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta
+		name="description"
+		content="Coraluna is a project that allows you to explore the Pharosverse."
+	/>
+	<meta
+		name="keywords"
+		content="Coraluna, Pharosverse, Coraluna NFT, Coraluna Game, Coraluna Metaverse"
+	/>
+	<meta name="author" content="Coraluna" />
+	<meta name="robots" content="index, follow" />
+	<meta name="googlebot" content="index, follow" />
+	<meta name="bingbot" content="index, follow" />
+	<meta name="yandexbot" content="index, follow" />
+	<title>Coraluna</title>
+	<meta name="title" content="Coraluna" />
+	<meta
+		name="description"
+		content="Coraluna is a project that allows you to explore the Pharosverse."
+	/>
+	<meta
+		name="keywords"
+		content="Coraluna, Pharosverse, Coraluna NFT, Coraluna Game, Coraluna Metaverse"
+	/>
+	<meta name="author" content="Coraluna" />
+	<meta name="robots" content="index, follow" />
+	<meta name="googlebot" content="index, follow" />
+	<meta name="bingbot" content="index, follow" />
+	<meta name="yandexbot" content="index, follow" />
 </svelte:head>
 
 {#key transitionKey}
